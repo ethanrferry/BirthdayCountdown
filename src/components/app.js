e8dc6d83cd377625e792886bbbe1b5937a0ad35e
Ethan Ferry <40361386+ethanrferry@users.noreply.github.com>
2018-08-28 16:08:11 -0600

    finished bday countdown app

--- src/components/app.js
+++ src/components/app.js
@@ -21,8 +21,9 @@ export default class App extends Component {
         hours: 0,
         minutes: 0,
         seconds: 0
-      }
-    }
+      },
+      age: 0
+    };
 
     this.handleGenerate = this.handleGenerate.bind(this)
   }
@@ -36,13 +37,42 @@ export default class App extends Component {
   }.bind(this)
 
   handleGenerate = function() {
-    this.setState({ active: true })
 
-    var countDownDate = this.state.startDate.toDate().getTime();
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
+
+    var timeBetween = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+    var age = Number((daysOld/365).toFixed(0));
+    this.setState({
+      age,
+      active: true
+      })
+
+
+    if (birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    }else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      }
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      }
+    }
+
+    var countDownDate = bday.getTime();
 
     this.timer  = setInterval(function() {
     
-      var now = new Date().getTime();
+      var now = moment().toDate().getTime();
 
       var distance = countDownDate - now;
 
@@ -66,17 +96,32 @@ export default class App extends Component {
     }.bind(this), 1000);
   }.bind(this)
 
+  getBirthDate = function(date) {
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    if(month < 10 ){
+      return`0${month}/${day}` 
+    }
+    return`${month}/${day}`
+  }.bind(this);
+
   renderItems = function() {
     if(this.state.active) {
       return [
-        <Clock timeRemaining={this.state.timeRemaining}/>,
+        <Clock key={0} timeRemaining={this.state.timeRemaining}/>,
         ChangeDate('Change Date', () => this.setState({ active: false })),
-        LargeText('04/03'),
-        <label className="grid__remaining">Remaining until your 18th birthday</label>
+        LargeText(this.getBirthDate(this.state.startDate.toDate())),
+        <label key={3} className="grid__remaining">
+          Remaining until you turn {this.state.age}
+        </label>
       ]
     } else {
       return [
-        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
+        <Picker
+        startDate={this.state.startDate} 
+        callback={(date) => this.handleChange(date)}
+        key={0}
+        />,
         Button('Generate Countdown', () => this.handleGenerate())
       ] 
     }
